05feae315e7d37df5c0fd40483344a6b454c7615
JuanCa291994 <jcrr9423@gmail.com>
2024-12-30 21:03:38 -0500

    create routes components

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MapComponent } from './map/map.component';
+import { MenusupComponent } from './menusup/menusup.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { AgendaComponent } from './agenda/agenda.component';
 
 export const routes: Routes = [
     {path:"",component:HomeComponent, pathMatch:"full"},
-    {path:"",component:DashboardComponent, pathMatch:"full"}
+    {path:"",component:DashboardComponent, pathMatch:"full"},
+    {path:"",component:MapComponent, pathMatch:"full"},
+    {path:"",component:MenusupComponent, pathMatch:"full"},
+    {path:"",component:CalendarComponent, pathMatch:"full"},
+    {path:"",component:AgendaComponent, pathMatch:"full"}
+
+
+
 
 ];
--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { MapComponent } from '../map/map.component';
+import { CalendarComponent } from '../calendar/calendar.component';
+import { MenusupComponent } from '../menusup/menusup.component';
+import { AgendaComponent } from '../agenda/agenda.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, MapComponent, CalendarComponent, MenusupComponent, AgendaComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
